2a4c8f265f4e704c813e635fef36a1afe59272dc
palgogo <ipalgogo@gmail.com>
2022-12-19 18:51:53 +0100

    add build gradle

---
+++ problems/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "1.7.0"
+    java
+}
+
+group = "com.palgogo"
+version = "unspecified"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
