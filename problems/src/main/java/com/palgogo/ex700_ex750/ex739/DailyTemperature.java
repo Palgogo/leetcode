c35e786edd207935d3571ce164cf37fa4a77de35
palgogo <ipalgogo@gmail.com>
2023-12-08 14:44:04 +0100

    solve 739 daily temperature.

---
+++ problems/src/main/java/com/palgogo/ex700_ex750/ex739/DailyTemperature.java
@@ -0,0 +1,22 @@
+package com.palgogo.ex700_ex750.ex739;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Stack;
+
+public class DailyTemperature {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        int[] result = new int[temperatures.length];
+        for (int i = temperatures.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && temperatures[i] >= temperatures[stack.peek()]) {
+                stack.pop();
+            }
+            if (!stack.isEmpty()) {
+                result[i] = stack.peek() - i;
+            }
+            stack.push(i);
+        }
+        return result;
+    }
+}
---
+++ problems/src/test/java/com/palgogo/ex700_ex750/ex739/DailyTemperatureTest.java
@@ -0,0 +1,21 @@
+package com.palgogo.ex700_ex750.ex739;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DailyTemperatureTest {
+
+    @Test
+    void dailyTemperatures() {
+        //given
+        int[] temperatures = {73,74,75,71,69,72,76,73};
+        int[] answers = {1,1,4,2,1,1,0,0};
+        //when
+        var dailyTemperature = new DailyTemperature();
+        int[] result = dailyTemperature.dailyTemperatures(temperatures);
+        //then
+        Assertions.assertArrayEquals(answers, result);
+    }
+}
