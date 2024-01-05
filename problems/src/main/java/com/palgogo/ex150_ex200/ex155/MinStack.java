995a97fcc0dd580f061cdbf5869911e697489197
palgogo <ipalgogo@gmail.com>
2024-01-05 11:28:23 +0100

    solve 155 min stack.

---
+++ problems/src/main/java/com/palgogo/ex150_ex200/ex155/MinStack.java
@@ -0,0 +1,43 @@
+package com.palgogo.ex150_ex200.ex155;
+
+public class MinStack {
+
+    private Node head;
+
+    public MinStack() {
+
+    }
+
+    public void push(int x) {
+        if (head == null) {
+            head = new Node(x, x, null);
+        } else {
+            head = new Node(x, Math.min(x, head.min), head);
+        }
+    }
+
+    public void pop() {
+        head = head.next;
+    }
+
+    public int top() {
+        return head.val;
+    }
+
+    public int getMin() {
+        return head.min;
+    }
+
+    private class Node {
+        int val;
+        int min;
+        Node next;
+
+        private Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+
+}
