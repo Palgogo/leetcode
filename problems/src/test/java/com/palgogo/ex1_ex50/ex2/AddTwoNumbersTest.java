7cc37abaf46a46c054fd134579de90f286504a1d
palgogo <ipalgogo@gmail.com>
2024-02-20 13:03:45 +0100

    solve add two numbers listnode

---
+++ problems/src/test/java/com/palgogo/ex1_ex50/ex2/AddTwoNumbersTest.java
@@ -0,0 +1,12 @@
+package com.palgogo.ex1_ex50.ex2;
+
+import org.junit.jupiter.api.Test;
+
+class AddTwoNumbersTest {
+
+    @Test
+    public void addTwoNumbers() {
+        AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
+        System.out.println(12 % 10);
+    }
+}
