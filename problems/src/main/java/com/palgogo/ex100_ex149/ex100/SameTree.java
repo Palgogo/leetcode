a0e57eda5d4a7060db6114e5e166aa003c051061
palgogo <ipalgogo@gmail.com>
2024-03-15 20:18:48 +0100

    add is the trees are the same solution.

---
+++ problems/src/main/java/com/palgogo/ex100_ex149/ex100/SameTree.java
@@ -0,0 +1,19 @@
+package com.palgogo.ex100_ex149.ex100;
+
+import com.palgogo.common.TreeNode;
+
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+        return false;
+    }
+}
